f5919ff22920cf27fd2c6655189f7cec0440acc4
Royxuzeng <e0376917@u.nus.edu>
2023-01-07 19:29:37 +0800

    add reset and slider to adjust grid size

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const grid = document.querySelector('.grid');
 
-createGrid = () => {
-    for (let i = 0; i < 256; i++) {
+createGrid = (val) => {
+    for (let i = 0; i < val * val; i++) {
         const div = document.createElement('div');
         div.classList.add('cell');
         div.addEventListener('mouseover', function(event){
@@ -11,4 +11,29 @@ createGrid = () => {
     }
 };
 
-createGrid();
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', function(){
+    let val = 16;
+    let cell = grid.children;
+    for (let i = 0; i < val*val; i++) {
+        cell[i].style.backgroundColor = 'white';
+    }
+});
+
+const slider = document.querySelector('#slider')
+const screenVal = document.querySelector('.value');
+slider.addEventListener('input', function(){
+    let val = document.getElementById('slider').value;
+    screenVal.textContent = val;
+    removeAllChildNodes(grid);
+    grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    createGrid(val);
+});
+
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+createGrid(16);
